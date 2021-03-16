c4507bfb0b866835fa75680df6d113d325a669e5
parogracz <57842866+parogracz@users.noreply.github.com>
2021-03-16 11:45:02 +0100

    Add files via upload

---
+++ kalendarzDB.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 15 Mar 2021, 23:19
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `kalendarz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `login` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `haslo` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`login`, `haslo`, `email`) VALUES
+('', '123', ''),
+('nowy ', 'nowy ', 'dn1@1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zadania`
+--
+
+CREATE TABLE `zadania` (
+  `login` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `nazwa` varchar(60) COLLATE utf8_polish_ci NOT NULL,
+  `priorytet` int(11) NOT NULL,
+  `data` int(11) NOT NULL,
+  `kolor` varchar(7) COLLATE utf8_polish_ci NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `zadania`
+--
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`login`);
+
+--
+-- Indeksy dla tabeli `zadania`
+--
+ALTER TABLE `zadania`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `zadania`
+--
+ALTER TABLE `zadania`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
